3f582dd2a775cdc45ca86be7a2c15295f6024ccf
ganfabo123-cmyk <ganfabo123@gmail.com>
2026-03-29 10:43:42 +0800

    remove .pyc

--- docx_manager/docx_helper/__init__.py
+++
@@ -1,6 +0,0 @@
-"""docx_manager 包"""
-
-from .api import process
-
-__all__ = ["process"]
-__version__ = "7.0.0"
--- docx_manager/full_style_docx_generator/main.py
+++
@@ -1,7 +0,0 @@
-from server import app
-
-if __name__ == '__main__':
-    """
-    Server starter for the full style docx generator
-    """
-    app.run(debug=True, port=5000)
